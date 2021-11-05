aebf7b090fd39f808192dd5857c8db77eac81d54
Nikita Prokopenko <guardprokopenko@gmail.com>
2021-11-05 15:33:11 +0300

    added attaches

--- src/test/java/tests/form/TestBase.java
+++ src/test/java/tests/form/TestBase.java
@@ -2,6 +2,7 @@ package tests.form;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
@@ -20,5 +21,13 @@ public class TestBase {
 
         Configuration.startMaximized = true;
         Configuration.baseUrl = "https://demoqa.com";
+    }
+
+    @AfterEach
+    public void tearDown() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
     }
 }
