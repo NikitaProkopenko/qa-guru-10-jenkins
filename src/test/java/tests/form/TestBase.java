b45b6042ec70e18aeeedacc70d68cae989958df0
Nikita Prokopenko <guardprokopenko@gmail.com>
2021-11-05 15:13:08 +0300

    added allure

--- src/test/java/tests/form/TestBase.java
+++ src/test/java/tests/form/TestBase.java
@@ -1,6 +1,8 @@
 package tests.form;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
 
 public class TestBase {
@@ -8,5 +10,6 @@ public class TestBase {
     static void beforeAll() {
         Configuration.startMaximized = true;
         Configuration.baseUrl = "https://demoqa.com";
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
     }
 }
